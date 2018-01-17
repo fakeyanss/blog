a2e5ff6d1b25f7707c478b1ecfef98cfcf7aceb2
fakeYanss <yanshisangc@gmail.com>
2018-01-17 20:05:21 +0800

    update blog

--- update.sh
+++ update.sh
@@ -1,1 +1,13 @@
-cd D:/Blog/Blog_Album && python tool.py && cd D:/Blog/Blog_Source && git add . && git commit -m 'update blog' && git push coding master && git rm -r --cached D:/Blog/Blog_Source/.daocloud && git commit -m 'update blog' && git push github master
+cd D:/Blog/Blog_Album && \
+python tool.py && \
+python upload-files-to-qiniu.py photos && \
+python upload-files-to-qiniu.py min_photos/ min_photos && \
+start move-to-album.bat && \
+python make-json.py && \
+cd D:/Blog/Blog_Source && \
+git add . && \
+git commit -m 'update blog' && \
+git push coding master && \
+git rm -r --cached D:/Blog/Blog_Source/.daocloud && \
+git commit -m 'update blog' && \
+git push github master
