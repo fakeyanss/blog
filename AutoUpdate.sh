d508bf6e65c3a6336d32541e53b3313eda12023c
fakeYanss <yanshisangc@gmail.com>
2018-02-07 13:35:20 +0800

    update blog

--- AutoUpdate.sh
+++ AutoUpdate.sh
@@ -1,9 +1,3 @@
-cd D:/Blog/Blog_Album && \
-python tool.py && \
-python upload-files-to-qiniu.py photos && \
-python upload-files-to-qiniu.py min_photos/ min_photos && \
-start move-to-album.bat && \
-python make-json.py && \
 cd D:/Blog/Blog_Source && \
 git add . && \
 git commit -m 'update blog' && \
