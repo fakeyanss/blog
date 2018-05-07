6bd5e73c5671b41a87dfc2ff23488099f201eed7
fakeYanss <yanshisangc@gmail.com>
2018-05-07 13:10:01 +0800

    update blog

--- AutoUpdate.sh
+++
@@ -1,7 +0,0 @@
-#cd D:/Blog/Blog_Source && \
-git add . && \
-git commit -m 'update blog' && \
-git push coding master && \
-git rm -r --cached .daocloud && \
-git commit -m 'update blog' && \
-git push github master
---
+++ release.sh
@@ -0,0 +1,5 @@
+#cd D:/Blog/Blog_Source && \
+git add . && \
+git commit -m 'update blog' && \
+git push all master
+#git rm -r --cached .daocloud
