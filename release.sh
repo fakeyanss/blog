0880d7f2b473b1a185611536ac1122d1dae4e43a
fakeYanss <yanshisangc@gmail.com>
2018-07-02 21:43:47 +0800

    update blog

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
-cd /Users/yanss/Documents/Blog/Blog_Source && \
-git add . && \
-git commit -m 'update blog' && \
+#!/bin/bash 
+cd /Users/yanss/Documents/Blog/Blog_Source
+git add .
+git commit -m 'update blog'
 git push origin master
