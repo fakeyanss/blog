056d9b575ab2cf234107ba6fa0678dcdf3eba7ee
fakeYanss <yanshisangc@gmail.com>
2019-03-23 11:11:48 +0800

    update blog

--- source/asset/sw.js
+++ source/asset/sw.js
@@ -3,7 +3,7 @@
   var cacheVersion = "-180503";
   var staticCacheName = "asset" + cacheVersion;
   var maxEntries = 100;
-  self.importScripts("https://foreti.me/asset/sw-toolbox.js");
+  self.importScripts("/asset/sw-toolbox.js");
   // self.importScripts("https://cdn.jsdelivr.net/npm/sw-toolbox@3.6.0/sw-toolbox.js");
   self.toolbox.options.debug = false;
   self.toolbox.options.networkTimeoutSeconds = 1;
